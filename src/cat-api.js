176f42de0d967bba178505816e7f79428d4ae240
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-15 18:20:14 +0100

    meh

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 export const fetchBreeds = () => {
   axios.defaults.headers.common['x-api-key'] =
-    'live_44KoTHoJ2869pUV9FwAO5e9D0JiJL83lJX8R3UNouJWAxwNjRYasMKJxe6ZXajcd';
+    'live_3WqxKBPPm7Nhb7bH6v5KJekaENAwu83GPEJU9IDPWyNtMxR4uV3pJr6uxDYogigc';
 
   return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
+};
+
+export const fetchCatByBreed = breedId => {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(res => res.data);
 };
