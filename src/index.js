d8ddc038bf9c463206da2f885228624c40311c3c
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-17 18:11:30 +0100

    hw10-cats-fetched

--- src/index.js
+++ src/index.js
@@ -1,42 +1,17 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import axios from 'axios';
 
-const breedSelect = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
+const fetchBreedsBtn = document.querySelector('.btn');
 
-try {
-  loader.classList.remove('hidden');
-  fetchBreeds().then(data => renderSelect(data));
-} catch (error) {
-  console.log(error);
-}
+fetchBreedsBtn.addEventListener('click', () => {
+  try {
+    axios.defaults.headers.common['x-api-key'] =
+      'live_44KoTHoJ2869pUV9FwAO5e9D0JiJL83lJX8R3UNouJWAxwNjRYasMKJxe6ZXajcd';
 
-function renderSelect(breeds) {
-  const markup = breeds
-    .map(({ id, name }) => {
-      return `<option value="${id}">${name}</option>`;
-    })
-    .join('');
-  breedSelect.insertAdjacentHTML('beforeend', markup);
-  loader.classList.add('hidden');
-}
-
-breedSelect.addEventListener('change', e => {
-  loader.classList.remove('hidden');
-  fetchCatByBreed(e.target.value).then(data => renderCat(data[0]));
+    axios
+      .get(`https://api.thecatapi.com/v1/breeds`)
+      .then(res => res.data)
+      .then(data => console.log(data));
+  } catch (error) {
+    console.log(error);
+  }
 });
-
-function renderCat(catData) {
-  const { url } = catData;
-  const { description, name, temperament } = catData.breeds[0];
-  catInfo.insertAdjacentHTML(
-    'beforeend',
-    `<div>
-        <h2>${name}</h2>
-        <img src="${url}" alt="${name}" />
-        <p>${description}</p>
-        <p><strong>Temperament:</strong> ${temperament}</p>
-    </div>`
-  );
-  loader.classList.add('hidden');
-}
