643d26953acea7185551f7ef3293d7228cc7e262
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-18 12:45:33 +0100

    hw10-part5

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 export const fetchBreeds = () => {
   axios.defaults.headers.common['x-api-key'] =
     'live_44KoTHoJ2869pUV9FwAO5e9D0JiJL83lJX8R3UNouJWAxwNjRYasMKJxe6ZXajcd';
 
-  return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
-  // .then(data => renderSelect(data));
+  return axios
+    .get(`https://api.thecatapi.com/v1/breeds`)
+    .then(res => res.data)
+    .catch(error => {
+      Notiflix.Notify.failure('Nie można pobrać listy ras.');
+      throw new Error('Nie można pobrać listy ras.');
+    });
 };
 
 export const fetchCatByBreed = breedId => {
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
+const errorElement = document.querySelector('.error');
+
+errorElement.classList.add('hidden');
+
+function handleError(error) {
+  Notiflix.Notify.failure(`Wystąpił błąd: ${error}`);
+  errorElement.classList.remove('hidden');
+  loader.classList.add('hidden');
+}
 
 try {
   loader.classList.remove('hidden');
-  fetchBreeds().then(data => renderSelect(data));
+  fetchBreeds()
+    .then(data => {
+      renderSelect(data);
+      const slim = new SlimSelect(breedSelect);
+    })
+    .catch(handleError);
 } catch (error) {
-  console.log(error);
+  handleError(error);
 }
 
 function renderSelect(breeds) {
+  breedSelect.innerHTML = '';
   breedSelect.insertAdjacentHTML(
     'beforeend',
     `<option value="start">---Choose your cat---</option>`
@@ -27,20 +45,29 @@ function renderSelect(breeds) {
 
 breedSelect.addEventListener('change', e => {
   loader.classList.remove('hidden');
-  fetchCatByBreed(e.target.value).then(data => renderCat(data[0]));
+  fetchCatByBreed(e.target.value)
+    .then(data => renderCat(data[0]))
+    .catch(handleError);
 });
 
 function renderCat(catData) {
+  errorElement.classList.add('hidden');
   catInfo.innerHTML = '';
   const { url } = catData;
   const { description, name, temperament } = catData.breeds[0];
   catInfo.insertAdjacentHTML(
     'beforeend',
     `<div>
+  
   <h2>${name}</h2>
+    <div class="breed-fluff">
   <img width = "800" src = ${url} alt = ${name}/>
+  <div class="desrc-temp">
+  <h4>Description</h4>
   <p>${description}</p>
-  <p>Temperament: ${temperament}</p>
+  <p><span class="temp-span">Temperament:</span> ${temperament}</p>
+  </div>
+  </div>
   </div>`
   );
   loader.classList.add('hidden');
